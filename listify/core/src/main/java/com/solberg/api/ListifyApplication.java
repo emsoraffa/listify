8bdb656e939b1c74705e8dccbce87481249ab873
emsoraffa <emil.rafael.solberg@gmail.com>
2024-06-11 12:48:31 +0200

    feat: setup spring boot and fix dependency management version

---
+++ listify/core/src/main/java/com/solberg/api/ListifyApplication.java
@@ -0,0 +1,12 @@
+package com.solberg.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListifyApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ListifyApplication.class, args);
+
+  }
+}
