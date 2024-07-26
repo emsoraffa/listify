8b3d57cb31815fed1cd6d1defc32cbd5b997bd78
emsoraffa <emil.rafael.solberg@gmail.com>
2024-07-26 22:07:45 +0200

    fix: data.sql

--- listify/core/src/main/resources/jdbc/data.sql
+++ listify/core/src/main/resources/jdbc/data.sql
@@ -1,16 +1,16 @@
 
 -- Insert mock user for development
-INSERT INTO users (name, email) VALUES ('Developer', 'dev@example.com');
+INSERT INTO users (id, name, email) VALUES (2, 'Developer', 'dev@example.com');
 
 -- Insert initial list for the mock user
-INSERT INTO listify_lists (user_id, list_name) VALUES (2, 'Min handleliste');
+INSERT INTO listify_lists (id, user_id, list_name) VALUES (2, 2, 'Min handleliste');
 
 -- Insert initial items for the mock user's list with position
-INSERT INTO list_items (name, state, position, listify_list_id) VALUES ('Batterier', FALSE, 1, (SELECT id FROM listify_lists WHERE list_name = 'Min handleliste'));
-INSERT INTO list_items (name, state, position, listify_list_id) VALUES ('Fisk', TRUE, 2, (SELECT id FROM listify_lists WHERE list_name = 'Min handleliste'));
-INSERT INTO list_items (name, state, position, listify_list_id) VALUES ('Pepsi', FALSE, 3, (SELECT id FROM listify_lists WHERE list_name = 'Min handleliste'));
-INSERT INTO list_items (name, state, position, listify_list_id) VALUES ('Yoghurt x2', FALSE, 4, (SELECT id FROM listify_lists WHERE list_name = 'Min handleliste'));
+INSERT INTO list_items (id, name, state, position, listify_list_id) VALUES (5, 'Batterier', FALSE, 1, 2);
+INSERT INTO list_items (id, name, state, position, listify_list_id) VALUES (6, 'Fisk', TRUE, 2, 2);
+INSERT INTO list_items (id, name, state, position, listify_list_id) VALUES (7, 'Pepsi', FALSE, 3, 2);
+INSERT INTO list_items (id, name, state, position, listify_list_id) VALUES (8, 'Yoghurt x2', FALSE, 4, 2);
 
 -- Insert initial collaborator for the mock user's list (assuming user with id 2)
-INSERT INTO list_collaborators (user_id, listify_list_id) VALUES (2, (SELECT id FROM listify_lists WHERE list_name = 'Min handleliste'));
+INSERT INTO list_collaborators (user_id, listify_list_id) VALUES (2, 2);
 
